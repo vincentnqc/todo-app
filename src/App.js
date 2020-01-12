7f625dd811cd672acc3daf7155a83328fae118d5
vincentnqc <vincentnqc92@gmail.com>
2020-01-11 18:17:43 -0600

    first push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isClicked: false,
+    text: "",
+    todos: []
+  }
+
+  addToDo = () => {
+  const todosCopy = this.state.todos
+  todosCopy.push(this.state.text)
+  this.setState({
+    todos: todosCopy
+  }) 
+  this.setState({
+    text: ''
+  })
+  }
+
+  deleteTodo = (index) => {
+    const todosCopy = this.state.todos
+    todosCopy.splice(index,1)
+    this.setState({
+      todos: todosCopy
+    }) 
+  }
+
+  updateText = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <h1>todoapp</h1>
+      <button onClick={this.addToDo}>addtodo</button>
+      <input onChange={this.updateText} value={this.state.text}></input>
+      {this.state.todos.map((item,index) => (
+    <p>{item}<button onClick ={()=>{this.deleteTodo(index)}}>x</button></p>
+      ))}
+      </div>
+    );
+  }
+
+
+ 
 }
 
 export default App;
